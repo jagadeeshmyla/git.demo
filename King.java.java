03f8f04fcac0d55d689669611455ec5978d77802
jagadeeshmyla <j9848367281@gmail.com>
2023-06-01 11:22:25 +0530

    king.java.java

---
+++ King.java.java
@@ -0,0 +1,8 @@
+class king
+{
+public static void main (String[]args)
+{
+ system.out.println("jagga");
+system.out.println("lucky");
+}
+}
---
+++ String.java
@@ -0,0 +1,10 @@
+class Name{
+public static void main(string[]args)
+{
+String s1="jaga";
+String s2="lucky";
+String s3=s1+s2;
+system.out.println(s3);
+}
+}
+
